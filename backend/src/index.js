d182f36d75cacfe705bfbc455deefc961226b4e5
JaiRaga <raga.j.santhosh@gmail.com>
2022-05-30 14:20:23 +0530

    :zap: setup graphql queries and mutation for user sign up

---
+++ backend/src/index.js
@@ -0,0 +1,105 @@
+const { ApolloServer, gql } = require('apollo-server');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const bcrypt = require('bcryptjs')
+// Load env vars
+const dotenv = require('dotenv');
+dotenv.config();
+
+const { DB_URI, DB_NAME } = process.env;
+
+const typeDefs = gql`
+  type Query {
+    myTaskLists: [TaskList!]!
+  }
+
+  type Mutation {
+    signUp(input: SignUpInput): AuthUser!
+    signIn(input: SignInInput): AuthUser!
+  }
+
+  input SignUpInput {
+    email: String!
+    password: String!
+    name: String!
+    avatar: String
+  }
+
+  input SignInInput {
+    email: String!
+    password: String!
+  }
+
+  type AuthUser {
+    user: User!
+    token: String!
+  }
+
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    avatar: String
+  }
+
+  type TaskList {
+    id: ID!
+    createdAt: String!
+    title: String!
+    progress: Float!
+
+    users: [User!]!
+    todos: [Todo!]!
+  }
+
+  type Todo {
+    id: ID!
+    content: String!
+    isCompleted: Boolean!
+
+    taskList: TaskList!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    myTaskLists: () => [],
+  },
+  Mutation: {
+    signUp: (_, {input}) => {
+      
+    },
+    signIn: () => {}
+  }
+};
+
+// Start the db
+const start = async () => {
+  const client = new MongoClient(DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+  });
+  await client.connect();
+  const db = client.db(DB_NAME);
+  console.log('connected to mongodb atlas 🚀');
+
+  const context = {
+    db,
+  };
+
+  // The ApolloServer constructor requires two parameters: your schema
+  // definition and your set of resolvers.
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context,
+    csrfPrevention: true,
+  });
+
+  // The `listen` method launches a web server.
+  server.listen().then(({ url }) => {
+    console.log(`Server ready at ${url} 🚀`);
+  });
+};
+
+start();
