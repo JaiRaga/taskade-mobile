2a25de6e8a8c9e12574fefcc9de53c48ff316412
JaiRaga <raga.j.santhosh@gmail.com>
2022-06-03 07:27:02 +0530

    :sparkles: displays checkbox to mark and unmark todos as complete

---
+++ taskade-mob/components/Checkbox/Checkbox.tsx
@@ -0,0 +1,22 @@
+import { View, Text, Pressable } from 'react-native';
+import React from 'react';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+interface CheckboxProps {
+  isChecked: boolean;
+  onPress: () => void;
+}
+
+const Checkbox = (props: CheckboxProps) => {
+  const { onPress, isChecked } = props;
+  const name = isChecked
+    ? 'checkbox-marked-outline'
+    : 'checkbox-blank-outline';
+  return (
+    <Pressable onPress={onPress}>
+      <MaterialCommunityIcons name={name} size={24} color="white" />
+    </Pressable>
+  );
+};
+
+export default Checkbox;
