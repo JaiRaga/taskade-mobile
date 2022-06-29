7ac6d84cc45fcfd26e74047f6fcc9b626f5ad95a
JaiRaga <44367062+JaiRaga@users.noreply.github.com>
2022-06-29 17:34:01 +0530

    :zap: Changed the screen name to ProjectsScreen

---
+++ taskade-mob/screens/ProjectsScreen.tsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import { StyleSheet, FlatList } from 'react-native';
+
+import EditScreenInfo from '../components/EditScreenInfo';
+import ProjectItem from '../components/ProjectItem';
+import { Text, View } from '../components/Themed';
+
+export default function ProjectsScreen() {
+  const [projects, setProjects] = useState([
+    {
+      id: '1',
+      title: 'project 1',
+      createdAt: '2d',
+    },
+    {
+      id: '2',
+      title: 'project 2',
+      createdAt: '3d',
+    },
+    {
+      id: '3',
+      title: 'project 3',
+      createdAt: '4d',
+    },
+  ]);
+
+  return (
+    <View style={styles.container}>
+      {/* Project task list */}
+      <FlatList
+        data={projects}
+        renderItem={({ item }) => <ProjectItem project={item} />}
+        style={styles.projects}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  projects: {
+    width: "100%"
+  }
+});
