6c86cb6cdba33ac8ab6c84661d66ddc5b066d425
JaiRaga <44367062+JaiRaga@users.noreply.github.com>
2022-07-02 17:36:51 +0530

    :zap: added token to the header of the request

--- taskade-mob/apollo.tsx
+++ taskade-mob/apollo.tsx
@@ -1,11 +1,27 @@
-import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Platform } from 'react-native';
 
 const URI =
   Platform.OS === 'ios' ? 'https://localhost:4000/' : 'http://10.0.2.2:4000/';
 
-export const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: URI,
+});
+
+const authLink = setContext(async (_, { headers }) => {
+  const token = await AsyncStorage.getItem('token');
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token || '',
+    },
+  };
+});
+
+export const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
-  
 });
